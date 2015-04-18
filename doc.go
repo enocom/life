5a07a67f2629debeb1f4b8a62ed96d22b85c82cf
Eno Compton 4 <eno4@ecom.com>
2015-04-18 12:30:05 -0600

    Add documentation

---
+++ doc.go
@@ -0,0 +1,16 @@
+/*
+
+Command life runs Conway's Game of Life in the terminal.
+
+Example Usage
+
+Start a simulation:
+
+    $ life
+
+Pass command line arguments:
+
+    $ life -gtime=500 -height=40 -width=40
+
+*/
+package main
