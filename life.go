c324df59d2ae42cc3a982e62a68c99a9232ef4a8
Eno Compton <eno4@ecom.com>
2017-07-19 13:02:04 -0600

    Formatting

--- life.go
+++ life.go
@@ -2,17 +2,17 @@
 //
 // The rules of the game are as follows:
 //
-// 1) Any live cell with fewer than two live neighbours dies,
-//    as if caused by underpopulation.
+// 1. Any live cell with fewer than two live neighbours dies,
+// as if caused by underpopulation.
 //
-// 2) Any live cell with two or three live neighbours lives
-//    on to the next generation.
+// 2. Any live cell with two or three live neighbours lives
+// on to the next generation.
 //
-// 3) Any live cell with more than three live neighbours dies,
-//    as if by overpopulation.
+// 3. Any live cell with more than three live neighbours dies,
+// as if by overpopulation.
 //
-// 4) Any dead cell with exactly three live neighbours becomes
-//    a live cell, as if by reproduction.
+// 4. Any dead cell with exactly three live neighbours becomes
+// a live cell, as if by reproduction.
 //
 // Typical usage is as follows:
 //     g := life.NewGame()
